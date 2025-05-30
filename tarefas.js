5791521a7946716e5bd8d02a1f8ffc36b93f8712
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-05-30 09:05:37 -0300

    Comece de agora o código

--- tarefas.js
+++ tarefas.js
@@ -1,1 +1,1 @@
-let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador', 'Limpar o teclado',];
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador', 'Limpar o teclado', 'Arrumar a sala de estar'];
