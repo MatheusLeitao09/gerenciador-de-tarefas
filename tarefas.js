ed9150aa0d4efbd14cf7e713a137cb9d6dcf8aa5
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-05-30 09:12:39 -0300

    Remove o último elemento

--- tarefas.js
+++ tarefas.js
@@ -1,8 +1,13 @@
 let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
 console.log(tarefas);
 
+
 // Adicionando dois elementos
 
 tarefas.push('Limpar o teclado', 'Arrumar a sala de estar')
-console.log(tarefas);
+console.log("Tarefas após adicionar dois elementos: " + tarefas);
+console.log('---------------------');
+// Removendo a ultima tarefa
 
+let tarefaRemovida = tarefas.pop();
+console.log("Tarefas depois de remover a última: " + tarefas);
